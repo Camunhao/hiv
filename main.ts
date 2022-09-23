5cca52e336f02eee5200bb867bc2517584dc5d1e
Camunhao <114233476+Camunhao@users.noreply.github.com>
2022-09-23 09:41:15 -0300

    Atualizar 'main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -1,3 +1,42 @@
-basic.forever(function () {
-	
+let p2 = 0
+let p1 = 0
+input.onPinPressed(TouchPin.P0, function () {
+    basic.showNumber(3)
+    basic.showNumber(2)
+    basic.showNumber(1)
+    basic.showString("bora")
+    basic.pause(randint(1000, 7000))
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . # # # .
+        . # # # .
+        . # # # .
+        `)
+    basic.pause(200)
+    basic.clearScreen()
+})
+input.onPinPressed(TouchPin.P2, function () {
+    p2 = 1
+    basic.showLeds(`
+        . # # # .
+        . . . # .
+        . # # # .
+        . # . . .
+        . # # # .
+        `)
+    basic.pause(100)
+    basic.clearScreen()
+})
+input.onPinPressed(TouchPin.P1, function () {
+    p1 = 1
+    basic.showLeds(`
+        . . # . .
+        . # # . .
+        . . # . .
+        . . # . .
+        . # # # .
+        `)
+    basic.pause(200)
+    basic.clearScreen()
 })
