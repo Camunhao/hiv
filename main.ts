076af8ac89d359589cf4aac9de2feda9b2d8ab9a
Camunhao <114233476+Camunhao@users.noreply.github.com>
2022-09-30 09:42:13 -0300

    Atualizar 'main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -1,11 +1,13 @@
+let tokar = false
 let p2 = 0
 let p1 = 0
 input.onPinPressed(TouchPin.P0, function () {
+    tokar = false
     basic.showNumber(3)
     basic.showNumber(2)
     basic.showNumber(1)
     basic.showString("bora")
-    basic.pause(randint(1000, 7000))
+    basic.pause(randint(1000, 10000))
     basic.showLeds(`
         . . # . .
         . . # . .
@@ -17,26 +19,63 @@ input.onPinPressed(TouchPin.P0, function () {
     basic.clearScreen()
 })
 input.onPinPressed(TouchPin.P2, function () {
-    p2 = 1
-    basic.showLeds(`
-        . # # # .
-        . . . # .
-        . # # # .
-        . # . . .
-        . # # # .
-        `)
-    basic.pause(100)
-    basic.clearScreen()
+    if (tokar == false) {
+        tokar = true
+        p2 = 1
+        basic.showLeds(`
+            . # # # .
+            . . . # .
+            . # # # .
+            . # . . .
+            . # # # .
+            `)
+        basic.pause(100)
+        basic.clearScreen()
+    }
 })
 input.onPinPressed(TouchPin.P1, function () {
-    p1 = 1
-    basic.showLeds(`
-        . . # . .
-        . # # . .
-        . . # . .
-        . . # . .
-        . # # # .
-        `)
-    basic.pause(200)
-    basic.clearScreen()
+    if (tokar == false) {
+        tokar = true
+        p1 = 1
+        basic.showLeds(`
+            . . # . .
+            . # # . .
+            . . # . .
+            . . # . .
+            . # # # .
+            `)
+        basic.pause(200)
+        basic.clearScreen()
+    }
+})
+basic.forever(function () {
+    if (p1 == 3) {
+        for (let index = 0; index < 3; index++) {
+            basic.showLeds(`
+                # . . . #
+                # # # # #
+                . . # . .
+                # . # . #
+                # . # . #
+                `)
+            basic.pause(500)
+        }
+        basic.showString("player 1 levou essa")
+        p2 = 0
+        p1 = 0
+    } else if (p2 == 3) {
+        for (let index = 0; index < 3; index++) {
+            basic.showLeds(`
+                # . . . #
+                # # # # #
+                . . # . .
+                # . # . #
+                # . # . #
+                `)
+            basic.pause(500)
+        }
+        basic.showString("player 2 levou essa")
+        p1 = 0
+        p2 = 0
+    }
 })
